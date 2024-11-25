24a3eea712196407961e7b2e1d72ba8663c396ed
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-11-25 18:55:58 +0200

    added actual calculation and memory to calculate the last number if a third calculation is requested.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let firstNum = "0";
 let operatorElement = "+";
-let thirdNum = "0";
+let secondNum = "0";
+let selection = 1;
+let shouldClear = false;
 
 function add(a, b) {
   return a + b;
@@ -18,29 +20,124 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(a, b, operator) {
-  if (operator === "+") {
-    add(a, b);
-  } else if (operator === "-") {
-    subtract(a, b);
-  } else if (operator === "*") {
-    multiply(a, b);
-  } else if (operator === "/") {
-    divide(a, b);
-  }
-}
-
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 
+function operate(a, b, operator) {
+  if (operator === "+") {
+    display.textContent = add(a, b);
+    return add(a, b);
+  } else if (operator === "-") {
+    display.textContent = subtract(a, b);
+    return subtract(a, b);
+  } else if (operator === "*") {
+    display.textContent = multiply(a, b);
+    return multiply(a, b);
+  } else if (operator === "/") {
+    display.textContent = divide(a, b);
+    return divide(a, b);
+  }
+}
+
+const clear = document.querySelector(".c");
+const plus = document.querySelector(".p");
+const minus = document.querySelector(".m");
+const star = document.querySelector(".st");
+const slash = document.querySelector(".sl");
+const equal = document.querySelector(".eq");
+
+clear.addEventListener("click", () => {
+  firstNum = "0";
+  operatorElement = "+";
+  secondNum = "0";
+  display.textContent = "";
+  selection = 1;
+});
+
+plus.addEventListener("click", () => {
+  operatorElement = "+";
+  if (selection === 1) {
+    selection = 2;
+    display.textContent = "";
+  } else if (selection === 2) {
+    firstNum = operate(
+      parseInt(firstNum),
+      parseInt(secondNum),
+      operatorElement
+    );
+    shouldClear = true
+  }
+});
+
+minus.addEventListener("click", () => {
+  operatorElement = "-";
+  if (selection === 1) {
+    selection = 2;
+    display.textContent = "";
+  } else if (selection === 2) {
+    firstNum = operate(
+      parseInt(firstNum),
+      parseInt(secondNum),
+      operatorElement
+    );
+    shouldClear = true
+  }
+});
+
+star.addEventListener("click", () => {
+  operatorElement = "*";
+  if (selection === 1) {
+    selection = 2;
+    display.textContent = "";
+  } else if (selection === 2) {
+    firstNum = operate(
+      parseInt(firstNum),
+      parseInt(secondNum),
+      operatorElement
+    );
+    shouldClear = true
+  }
+});
+
+slash.addEventListener("click", () => {
+  operatorElement = "/";
+  if (selection === 1) {
+    selection = 2;
+    display.textContent = "";
+  } else if (selection === 2) {
+    firstNum = operate(
+      parseInt(firstNum),
+      parseInt(secondNum),
+      operatorElement
+    );
+    shouldClear = true
+  }
+});
+
+equal.addEventListener("click", () => {
+  if (selection === 2) {
+    operate(parseInt(firstNum), parseInt(secondNum), operatorElement);
+    selection = 1;
+  }
+});
+
 buttons.forEach((button) => {
-  if (!button.classList.contains('empty')) {
+  if (!button.classList.contains("empty")) {
     button.addEventListener("click", () => {
       addToDisplay(button.textContent);
     });
-  };
+  }
 });
 
 function addToDisplay(number) {
+  if (shouldClear) {
+    display.textContent = "";
+    shouldClear = false;
+  }
   display.textContent += number;
+  if (selection === 1) {
+    firstNum = display.textContent;
+  } else {
+    secondNum = display.textContent;
+  }
 }
