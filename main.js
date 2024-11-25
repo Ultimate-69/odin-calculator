2559e6ed6518e0f79aad9d986144c84f1e13add9
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-11-25 17:16:35 +0200

    added basic functions & variables

---
+++ main.js
@@ -0,0 +1,20 @@
+let firstNum = 0;
+let operatorElement = "+";
+let thirdNum = 0;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
